661a362071a0f697f7a6b6e090e2442fd4d0fd81
goldendragon0830 <goldendragon0830@gmail.com>
2024-08-09 22:15:02 -0400

    fix error

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -7,10 +7,10 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ height: '200px', objectFit: 'cover' }} />
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" style={{ height: '100px', objectFit: 'cover' }} />
       <Card.Body>
         <Card.Title style={{ height: "50px" }}>{props.title}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{props.role}</Card.Subtitle>
+        <Card.Subtitle style={{ color: 'rgb(195, 0, 0)' }} className="mb-2 text-muted">{props.role}</Card.Subtitle>
         <Card.Text style={{ textAlign: "justify", height: "300px", overflowY: "auto" }}>
           {props.description}
         </Card.Text>
